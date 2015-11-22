c1a5df5349a4d95555fd454157fc6a85cbee7320
xtornasol512 <xtornasol512@gmail.com>
2015-11-22 17:57:06 -0600

    Static files and frontend

---
+++ frontend/js/main.js
@@ -0,0 +1,10 @@
+$(document).foundation();
+
+$(".owl-carousel").owlCarousel({
+    items: 1,
+    autoplay:true,
+    autoplayTimeout:6000,
+    loop:true,
+    animateOut: 'fadeOut',
+    nav: true
+});
